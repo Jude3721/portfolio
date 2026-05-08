f97ddb2b30fef27fa014657cde5806420b120194
jdkim <judae3721@gmail.com>
2026-05-08 19:15:50 +0900

    fix: 채팅 CORS POST 허용, CI에 Render 배포 훅 추가

--- nba-dashboard/server/index.js
+++ nba-dashboard/server/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin
   if (ALLOWED_ORIGINS.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin)
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   }
   if (req.method === 'OPTIONS') return res.sendStatus(204)
